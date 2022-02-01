9f9ab9fc5819966849ab39ceb01c68476109f2ac
jengmunw <wongjm4@hotmail.com>
2022-02-01 21:11:41 +0800

    basic model setup

---
+++ proj4/account/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, address, postal_code, password=None):
+        if not email:
+            raise ValueError('Please input email address.')
+
+        user = self.model(email=self.normalize_email(email),
+                          first_name=first_name,
+                          last_name=last_name,
+                          address=address,
+                          postal_code=postal_code)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    address = models.CharField(max_length=100)
+    postal_code = models.PositiveIntegerField()
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'address', 'postal_code']
+
+    def __str__(self):
+        return self.email
---
+++ proj4/inventory/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+
+
+class Inventory(models.Model):
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    image = models.CharField(max_length=150)
+    price = models.PositiveSmallIntegerField()
+    cost = models.PositiveIntegerField()
+    quantity = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
