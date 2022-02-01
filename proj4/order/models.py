0400370d948aba241df31f3f14be3ca0a3b301ef
jengmunw <wongjm4@hotmail.com>
2022-02-01 22:58:17 +0800

    added models for order item

--- proj4/order/models.py
+++ proj4/order/models.py
@@ -1,13 +1,23 @@
 import uuid
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-# class Order(models.Model):
-#     order_no = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-#     date = models.DateTimeField(auto_now_add=True)
-#     customer = models.ForeignKey('account.Account', on_delete=models.DO_NOTHING())
-#     order_details = ArrayField(models.JSONField())
-#
-#     def __str__(self):
-#         return self.order_no
+class Order(models.Model):
+    order_no = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey('account.Account', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.order_no
+
+
+class OrderItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    price = models.PositiveSmallIntegerField()
+    cost = models.PositiveIntegerField()
+    quantity = models.PositiveSmallIntegerField()
+    item = models.ForeignKey('inventory.Inventory', on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
