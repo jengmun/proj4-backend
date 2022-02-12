25f5d7fc1de799310aac4e50fc7e7e254771ab14
jengmun <92924947+jengmun@users.noreply.github.com>
2022-02-12 13:17:07 +0800

    .env config

--- proj4/proj4/settings.py
+++ proj4/proj4/settings.py
@@ -13,6 +13,7 @@ import os
 from datetime import timedelta
 from pathlib import Path
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ken%edcuk(o=-l57k3#2g)38$1zr#6)tn)xeq5gd#+a+k#iueb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
